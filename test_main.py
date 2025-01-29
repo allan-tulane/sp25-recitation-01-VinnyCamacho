65fd833577062701991f463908415e4b5a35a169
VinnyCamacho <vcamacho1@tulane.edu>
2025-01-29 13:57:36 -0600

    Update test_main.py
    done

--- test_main.py
+++ test_main.py
@@ -15,7 +15,6 @@ def test_binary_search():
 	assert binary_search([1,2,3,4,5], 6) == -1
 	assert binary_search([1,2,3,4,5], 7) == -1
 	assert binary_search([1,2,3,4,5], 3) == 2
-	### TODO: add two more tests here.
 
 	###
 
